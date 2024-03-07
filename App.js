4fff574cc5ab4713448fd0dc718a80016f5da717
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-07 13:11:27 -0300

    Adicionado tela RecoveryPassWord

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Splash from "./src/screens/Splash";
 import Login from "./src/screens/Login";
+import RecoveryPassWord from "./src/screens/RecoveryPassword";
 
 const Stack = createNativeStackNavigator();
 
@@ -26,9 +27,11 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Splash"
+        screenOptions={{headerShown:false}}
       >
         <Stack.Screen name='Splash' component={Splash} />
         <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='RecoveryPassWord' component={RecoveryPassWord} />
       </Stack.Navigator>
     </NavigationContainer>
   );
