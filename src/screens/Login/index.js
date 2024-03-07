1877c6789abf249490815919161e404ef3881671
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-07 00:54:51 -0300

    Criado Tela Login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,7 +1,46 @@
+import { BrandLogoBlue } from "../../components/BrandLogo/style";
+import { ButtonDefault, ButtonGoogle } from "../../components/Buttons";
+import { Container, ContainerMargin } from "../../components/Conatainer";
+import { InputGreen } from "../../components/Inputs/styled";
+import { LinkGray } from "../../components/Links";
+import { Title } from "../../components/Texts/style";
 
 
-export default function Login () {
-  return(
-    <></>
+export default function Login() {
+  return (
+    <Container>
+
+      <ContainerMargin $mb={25}>
+        <BrandLogoBlue />
+      </ContainerMargin>
+      <Title>Entrar ou criar conta</Title>
+      <ContainerMargin $gap={15} $mt={20} $mb={10}>
+        <InputGreen
+          placeholder="Usuário ou E-mail"
+          enterKeyHint="next"
+          keyboardType="email-address"
+          maxLength={50}
+        />
+
+        <InputGreen
+          placeholder="Senha"
+          enterKeyHint="enter"
+          keyboardType="default"
+          maxLength={50}
+          secureTextEntry={true}
+        />
+      </ContainerMargin>
+
+      <LinkGray>Esqueceu sua senha?</LinkGray>
+
+      <ContainerMargin $mt={42} $gap={15} $mb={30}>
+        <ButtonDefault textButton="Entrar" />
+        <ButtonGoogle textButton="Entrar com google"/>
+      </ContainerMargin>
+
+
+    </Container>
+
+
   )
 } 
