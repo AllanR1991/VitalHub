a5f6290676d50e75b82f3e03362398e2c80807ae
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-06 21:11:28 -0300

    Criado navegação entre as telas e setado as fontes do projeto.

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import { useFonts, MontserratAlternates_500Medium, MontserratAlternates_600SemiBold, MontserratAlternates_700Bold } from "@expo-google-fonts/montserrat-alternates";
 import { Quicksand_400Regular, Quicksand_500Medium, Quicksand_600SemiBold } from "@expo-google-fonts/quicksand";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Splash from "./src/screens/Splash";
+import Login from "./src/screens/Login";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
 
   const [fontsLoaded, fontError] = useFonts({
@@ -16,6 +23,13 @@ export default function App() {
   }
 
   return (
-    <></>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Splash"
+      >
+        <Stack.Screen name='Splash' component={Splash} />
+        <Stack.Screen name='Login' component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
