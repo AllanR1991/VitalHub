586579a7a7df6c96e9fc3a8e0f0ded70e6f77557
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-07 13:13:34 -0300

    Criado linkBlueligth

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -9,4 +9,13 @@ export const LinkGray = styled.Text`
   text-align:left;
   
   color: #8C8A97;
+  padding: 4px 0px;
+`
+
+export const LinkBlueLigth = styled.Text`
+  font-size: 14px;
+  font-family:'MontserratAlternates_600SemiBold';
+  color: #4D659D;
+  text-decoration: underline;
+  padding: 4px 0px;
 `
