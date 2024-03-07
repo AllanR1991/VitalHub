50df67bbe21650ab1983398169920518017e3fc3
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-07 00:53:49 -0300

    Criado componente LinkGray

---
+++ src/components/Links/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const LinkGray = styled.Text`
+  width:90%;
+  
+  font-family: 'MontserratAlternates_500Medium';
+  font-size: 14px;
+  text-decoration:underline;
+  text-align:left;
+  
+  color: #8C8A97;
+`
