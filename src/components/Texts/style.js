b558bbea0179170849576987b0aae3b3a2c9e36e
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-06 21:12:29 -0300

    Criado componete TextSlogan

---
+++ src/components/Texts/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components/native"
+
+export const TextSlogan = styled.Text`
+  font-family: 'Quicksand_600SemiBold';
+  color: #fff;
+  font-size: 18px;
+  text-align:center;
+  width:203px;
+`
