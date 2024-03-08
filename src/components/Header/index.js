cd2626c9fa1e03a56f026df3b3364cd5f7f234b8
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-08 06:10:06 -0300

    Inputando dados mokados e inserido ImageUser

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,24 @@
 import { ContainerHeader } from "../Conatainer"
+import { ImageUser } from "../Images/style"
 import { LinearGradienteHeader } from "./style"
+
+const Dados = [
+  {id:1, nome: 'Allan Rodrigues dos Santos', email: 'allan@allan.com', senha: 'admin', image: 'https://github.com/AllanR1991.png', typeUser: 'admin'},
+  {id:2, nome: 'Carlos', email: 'carlos@carlos.com', senha: 'user', image: 'https://github.com/Carlos-Augusto-Roque.png', typeUser: 'paciente'},
+  {id:3, nome: 'Evelin', email: 'evelin@evelin.com', senha: 'user', image: 'https://github.com/evy-oliveira0807.png', typeUser: 'paciente'},
+]
+
 
 export const Header = (
   {
 
   }
 ) => {
+
   return(
     <ContainerHeader>
       <LinearGradienteHeader>
-        
+        <ImageUser source={{uri: 'https://github.com/AllanR1991.png'}}/>
       </LinearGradienteHeader>
     </ContainerHeader>
   )
