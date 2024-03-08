f8bfb7067e4696b9e185a9eb25ed325761a77a22
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-08 06:10:26 -0300

    Criado componente ImageUser

---
+++ src/components/Images/style.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+interface ImageUserProps {
+  $width: string;
+  $height: string;
+  $borderRadius: string;
+}
+
+export const ImageUser = styled.Image<ImageUserProps>`
+  width: ${props => props.$width !== undefined ? `${props.$width}` : '60px'};
+  height: ${props => props.$height !== undefined ? `${props.$height}` : '60px'};
+  border-radius: ${props => props.$borderRadius !== undefined ? `${props.$borderRadius}` : '5px'};
+  
+`
