388e67b4b56d62cee15cf53d634adcf1483b4504
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-18 23:19:13 -0300

    Desenvolvido a tela Home com header, botoes de seleção e cards

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,13 +1,89 @@
-import { StatusBar } from "react-native";
-import { Container } from "../../components/Conatainer";
+import { FlatList, StatusBar, View } from "react-native";
+import { Container, ContainerMargin } from "../../components/Conatainer";
 import { Header } from "../../components/Header";
 
-export default function Home () {
-  return(
-    <Container>
-      <StatusBar  translucent={true} barStyle="light-content" backgroundColor={'transparent'}/>
+import { CalendarListWeek } from "../../components/Calendars";
+import { ButtonNotSelect, ButtonSelect } from "../../components/Buttons";
+import { useState } from "react";
+import CardAppointment from "../../components/CardAppointment";
+import { ModalCancel, ModalMedicalRecord } from "../../components/Modals";
 
-      <Header/>
+export default function Home() {
+
+  const [select, setSelect] = useState('Agendadas');
+
+  const [showModalCancel, setShowModalCancel] = useState(false);
+  const [showModalMedicalRecord, setShowModalMedicalRecord] = useState(false);
+  const [consultSelect, setConsultSelect] = useState(null);
+  // const [dataTest, setDataTeste] = useState(dataPacient);
+
+  const statusConsult = ['Agendadas', 'Realizadas', 'Canceladas'];
+
+  const dataPacient = [
+    { id: 1, name: 'Allan Rodrigues dos Santos', age: 32, email: 'allan@allan.com', timeConsult: '15:00', typeConsult: 'Exame', statusConsult: 'Realizadas', photo: 'https://github.com/AllanR1991.png' },
+    { id: 2, name: 'Everton Araujo', age: 35, email: 'eveton@everton.com', timeConsult: '12:00', typeConsult: 'Rotina', statusConsult: 'Agendadas', photo: 'https://github.com/Evertonaraujo88.png' },
+    { id: 3, name: 'Everton Araujo', age: 35, email: 'eveton@everton.com', timeConsult: '12:00', typeConsult: 'Rotina', statusConsult: 'Agendadas', photo: 'https://github.com/Evertonaraujo88.png' },
+    { id: 4, name: 'Everton Araujo', age: 35, email: 'eveton@everton.com', timeConsult: '12:00', typeConsult: 'Rotina', statusConsult: 'Agendadas', photo: 'https://github.com/Evertonaraujo88.png' },
+    { id: 5, name: 'Everton Araujo', age: 35, email: 'eveton@everton.com', timeConsult: '12:00', typeConsult: 'Rotina', statusConsult: 'Agendadas', photo: 'https://github.com/Evertonaraujo88.png' },
+    { id: 6, name: 'Everton Araujo', age: 35, email: 'eveton@everton.com', timeConsult: '12:00', typeConsult: 'Rotina', statusConsult: 'Agendadas', photo: 'https://github.com/Evertonaraujo88.png' },
+    { id: 7, name: 'Everton Araujo', age: 35, email: 'eveton@everton.com', timeConsult: '12:00', typeConsult: 'Rotina', statusConsult: 'Agendadas', photo: 'https://github.com/Evertonaraujo88.png' },
+    { id: 8, name: 'Kamille Milo', age: 20, email: 'kamille@kamille.com', timeConsult: '13:00', typeConsult: 'Exame', statusConsult: 'Canceladas', photo: 'https://github.com/KamiMilo.png' }
+  ]
+
+
+  return (
+    <Container $bgColor="#fbfbfb">
+      <StatusBar translucent={true} barStyle="light-content" backgroundColor={'transparent'} />
+
+      <Header />
+
+      <ContainerMargin $mt={20}>
+        <CalendarListWeek />
+      </ContainerMargin>
+
+      <ContainerMargin $fd="row" $justContent="space-between" $mt={38}>
+        {statusConsult.map((status, index) => (
+          <ButtonSelect key={index} selectStatus={select === status} onPress={() => { setSelect(status) }} texto={status} />
+        ))}
+      </ContainerMargin>
+
+      <ContainerMargin $mt={24}>
+        <FlatList
+          data={dataPacient}
+          renderItem={({ item }) =>
+            select == item.statusConsult && (
+              <CardAppointment
+                data={item}
+                selectStatus={select}
+                setShowModalCancel={setShowModalCancel}
+                setShowModalMedicalRecord={setShowModalMedicalRecord}
+                setConsultSelect={setConsultSelect}
+              />
+            )
+          }
+          keyExtractor={item => item.id}
+          style={{
+            width: '100%',
+          }}
+          showsVerticalScrollIndicator={false}
+        />
+
+
+      </ContainerMargin>
+
+      <ModalCancel
+        consultSelect={consultSelect}
+        data={dataPacient}
+        setShowModalCancel={setShowModalCancel}
+        showModalCancel={showModalCancel}
+      />
+      <ModalMedicalRecord
+        consultSelect={consultSelect}
+        setShowModalMedicalRecord={setShowModalMedicalRecord}
+        showModalMedicalRecord={showModalMedicalRecord}
+      />
     </Container>
+
+
   )
 }
