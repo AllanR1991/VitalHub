0498aebade6467b0ab829bbe6a04371195440015
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-18 23:15:55 -0300

    Alterado para usar image do expo-image

--- src/components/Images/style.tsx
+++ src/components/Images/style.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { Image } from 'expo-image'
 
 interface ImageUserProps {
   $width: string;
@@ -6,7 +7,7 @@ interface ImageUserProps {
   $borderRadius: string;
 }
 
-export const ImageUser = styled.Image<ImageUserProps>`
+export const ImageUser = styled(Image)<ImageUserProps>`
   width: ${props => props.$width !== undefined ? `${props.$width}` : '60px'};
   height: ${props => props.$height !== undefined ? `${props.$height}` : '60px'};
   border-radius: ${props => props.$borderRadius !== undefined ? `${props.$borderRadius}` : '5px'};
