233060508106cca4ec8b434f2551384f51e29691
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-18 23:15:14 -0300

    Alterado padding para porcentagem

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import { ContainerHeader, ContainerMargin } from "../Conatainer"
 import { ImageUser } from "../Images/style"
-import { TextNameUser, TitleHeader } from "../Texts/style"
+import { TextNameUserWhite, TitleHeader } from "../Texts/style"
 import { LinearGradienteHeader } from "./style"
 import { Ionicons } from '@expo/vector-icons';
 
@@ -16,12 +16,12 @@ export const Header = (
   return (
     <ContainerHeader>
       <LinearGradienteHeader>
-        <ContainerMargin $fd="row" $justContent="space-between" $width="100%" $pd="62px 20px 22px 20px">
+        <ContainerMargin $fd="row" $justContent="space-between" $width="100%" $pd="62px 5% 22px 5%">
           <ContainerMargin $fd="row" $gap={10} $width="auto">
             <ImageUser source={{ uri: 'https://github.com/AllanR1991.png' }} />
             <ContainerMargin $alingItens="flex-start" $justContent="flex-start" $width="auto" $gap={3}>
               <TitleHeader>Bem vindo!</TitleHeader>
-              <TextNameUser>Allan</TextNameUser>
+              <TextNameUserWhite>Allan</TextNameUserWhite>
             </ContainerMargin>
           </ContainerMargin>
 
