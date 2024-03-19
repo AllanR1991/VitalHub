e706320eebbb32d753541fb7a4027a1a622c26fe
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-19 12:49:30 -0300

    Alterado barStyle

--- src/screens/MedicalRecord/index.js
+++ src/screens/MedicalRecord/index.js
@@ -6,7 +6,7 @@ export default MedicalRecord = () => {
   return (
     <Container>
 
-      <StatusBar translucent={true} barStyle="dark-content" backgroundColor={'transparent'} currentHeight />
+      <StatusBar translucent={true} barStyle="dark" backgroundColor={'transparent'} currentHeight />
 
       <ImageUser source={{uri: 'https://github.com/AllanR1991.png'}} $width="100%" $height="280"/>
 
