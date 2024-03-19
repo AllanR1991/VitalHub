d3b225edea27b8101829cf05ec66d30287964619
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-18 23:18:26 -0300

    Craido componentes TextQuickSandRegular, NivelConsult, TextTime e TextNameUserBlack

--- src/components/Texts/style.js
+++ src/components/Texts/style.js
@@ -1,4 +1,10 @@
-import styled from "styled-components/native"
+import styled, { css } from "styled-components/native"
+
+export const TextQuickSandRegular = styled.Text`
+  font-family: 'Quicksand_400Regular';
+  font-size: 14px;
+  color: #8C8A97;
+`
 
 export const Description = styled.Text`
   font-family: 'Quicksand_500Medium';
@@ -9,8 +15,11 @@ export const Description = styled.Text`
 
 export const TitleHeader = styled(Description)`
   font-size: 14px;
-  text-align: left;
   color: #4E4B59;
+`
+
+export const NivelConsult = styled(TitleHeader)`
+  color: #8C8A97;
 `
 
 export const TextSlogan = styled.Text`
@@ -21,6 +30,32 @@ export const TextSlogan = styled.Text`
   width:203px;
 `
 
+export const TextTime = styled.Text`
+  ${({selectStatus}) => {
+    if(selectStatus){
+      return css`
+        color: #49B3BA;
+      `
+    }else{
+      return css`
+        color: #4E4B59;
+      `
+    }
+  }}
+  font-family: 'Quicksand_600SemiBold';
+  font-size: 14px;
+`
+
+export const TextCancelAppointment = styled.Text`
+  font-family:'MontserratAlternates_500Medium';
+  color:  #c81d25;
+  font-size: 12px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+`
+export const TextPontuarioAppointment = styled(TextCancelAppointment)`
+  color: #344F8F;
+`
 export const Title = styled.Text`
   font-family:'MontserratAlternates_600SemiBold';
   font-size: 20px;
@@ -33,10 +68,27 @@ export const TextGrayDark = styled.Text`
   color: #4E4B59;
 `
 
-export const TextNameUser = styled(TextGrayDark)`
+export const TextNameUserWhite = styled(TextGrayDark)`
   font-size: 16px;
   color: #FBFBFB;
 `
+
+export const TextNameUserBlack = styled(TextNameUserWhite)`
+  color: #33303E;
+`
+
+export const TextSelect = styled(TextNameUserWhite)`
+  ${({ selectStatus }) => {
+    if (!selectStatus) {
+      return css`
+        color: #607EC5;      
+      `
+    }
+  }}
+  font-size: 12px; 
+  text-align:center;
+`
+
 
 export const TextButtonBlue = styled.Text`
   font-family: 'MontserratAlternates_700Bold';
