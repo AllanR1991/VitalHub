e7506d9a0a6b5b0782ff2fe5a574d2b6c56be938
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-20 05:41:26 -0300

    Recebido os parametros do navigation

--- src/screens/MedicalRecord/index.js
+++ src/screens/MedicalRecord/index.js
@@ -1,14 +1,37 @@
-import { StatusBar } from "expo-status-bar"
-import { Container } from "../../components/Conatainer"
-import { ImageUser } from "../../components/Images/style"
 
-export default MedicalRecord = () => {
+import { StatusBar } from "react-native"
+import { Container, ContainerMargin } from "../../components/Conatainer"
+import { ImageUser } from "../../components/Images/style"
+import { TextQuickSandRegular, Title } from "../../components/Texts/style"
+
+export default MedicalRecord = ({
+  navigation,
+  route
+}) => {
+
+  const {name, age, email, photo} = route.params
+
   return (
     <Container>
 
-      <StatusBar translucent={true} barStyle="dark" backgroundColor={'transparent'} currentHeight />
+      <StatusBar translucent={true} barStyle="light-content" backgroundColor={'transparent'} currentHeight />
 
-      <ImageUser source={{uri: 'https://github.com/AllanR1991.png'}} $width="100%" $height="280"/>
+      <ImageUser source={photo !== undefined ? { uri: photo } : require('../../assets/images/NotImage.svg')} $width="100%" $height="280px" />
+
+      <ContainerMargin $mt={20} $width="100%">
+        <Title>
+          {name}
+        </Title>
+      </ContainerMargin>
+      <ContainerMargin $width="80%" $mt={18} $fd="row" $justContent="space-around">
+        <TextQuickSandRegular>
+          {age}
+        </TextQuickSandRegular>
+        <TextQuickSandRegular>
+          {email}
+        </TextQuickSandRegular>
+      </ContainerMargin>
+
 
     </Container>
   )
