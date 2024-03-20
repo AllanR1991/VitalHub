4b4461ede2d0bb019116d500f51a30e554cb20ec
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-20 06:31:40 -0300

    Inserido o scrollView os labels e os inputs

--- src/screens/MedicalRecord/index.js
+++ src/screens/MedicalRecord/index.js
@@ -1,8 +1,9 @@
 
 import { StatusBar } from "react-native"
-import { Container, ContainerMargin } from "../../components/Conatainer"
+import { Container, ContainerMargin, ContainerScrollView } from "../../components/Conatainer"
 import { ImageUser } from "../../components/Images/style"
-import { TextQuickSandRegular, Title } from "../../components/Texts/style"
+import { TextLabel, TextQuickSandRegular, Title } from "../../components/Texts/style"
+import { InputGreen, InputGreenMultiLine } from "../../components/Inputs/styled"
 
 export default MedicalRecord = ({
   navigation,
@@ -18,6 +19,11 @@ export default MedicalRecord = ({
 
       <ImageUser source={photo !== undefined ? { uri: photo } : require('../../assets/images/NotImage.svg')} $width="100%" $height="280px" />
 
+      <ContainerScrollView
+        showsVerticalScrollIndicator={false}
+      >
+
+      
       <ContainerMargin $mt={20} $width="100%">
         <Title>
           {name}
@@ -32,7 +38,21 @@ export default MedicalRecord = ({
         </TextQuickSandRegular>
       </ContainerMargin>
 
+      <ContainerMargin $alingItens="flex-start">
+        <TextLabel>Descrição da consulta</TextLabel>
+        <InputGreenMultiLine placeholder="Descrição"/>
+      </ContainerMargin>
 
+      <ContainerMargin $alingItens="flex-start">
+        <TextLabel>Diagnóstico do paciente</TextLabel>
+        <InputGreen placeholder="Diagnóstico"/>
+      </ContainerMargin>
+
+      <ContainerMargin $alingItens="flex-start">
+        <TextLabel>Prescrição médica</TextLabel>
+        <InputGreenMultiLine placeholder="Prescrição medica"/>
+      </ContainerMargin>      
+      </ContainerScrollView>
     </Container>
   )
 }
