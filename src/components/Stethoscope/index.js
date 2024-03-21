62870a5abc8b7f201b212a5cf46c5fcdb7dfb163
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-03-20 22:11:16 -0300

    Adicionado paramento onPress e mudado ContainerMargin para ButtonStethoscope

--- src/components/Stethoscope/index.js
+++ src/components/Stethoscope/index.js
@@ -1,10 +1,14 @@
+import { ButtonStethoscope } from "../Buttons/style";
 import { ContainerMargin } from "../Conatainer"
 import { FontAwesome6 } from '@expo/vector-icons';
 
-export const Stethoscope = () => {
+export const Stethoscope = ({
+  onPress
+}) => {
   return (
-    <ContainerMargin $bgColor="#49B3BA" $height="60px" $width="60px" $borderRadius={7} style={{position: "absolute", bottom:(0+56+15), right: 21 }}>
-      <FontAwesome6 name="stethoscope" size={32} color="#FFF" />
-    </ContainerMargin>
+    <ButtonStethoscope onPress={onPress}>
+          <FontAwesome6 name="stethoscope" size={32} color="#FFF" />
+    </ButtonStethoscope>
+    
   )
 }
